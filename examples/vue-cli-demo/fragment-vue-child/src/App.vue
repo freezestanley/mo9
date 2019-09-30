37cd1264ecba7623917d1d14e983a5eb11088ecc
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-09-30 17:26:01 +0800

    xx

---
+++ examples/vue-cli-demo/fragment-vue-child/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div>This is fragment-vue-child</div>
+    <div @click="push">switch child hello</div>
+    <div @click="push2">switch child world</div>
+    <div>==============================================</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  methods: {
+    push() {
+      this.$router.push({
+        path: 'hello'
+      })
+    },
+    push2() {
+      this.$router.push({
+        path: 'world'
+      })
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
