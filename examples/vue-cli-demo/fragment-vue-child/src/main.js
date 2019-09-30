da91f993f345635e7b0c876c6b4204415a9203df
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-09-30 17:39:14 +0800

    xx

--- examples/vue-cli-demo/fragment-vue-child/src/main.js
+++ examples/vue-cli-demo/fragment-vue-child/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import Router from './router'
 
 Vue.config.productionTip = false
+console.log('NODE_ENV=' + JSON.stringify(process.env))
 
-new Vue({
-  router: Router('child'),
-  render: h => h(App),
-}).$mount('#app')
+if (process.env.NODE_ENV === 'development') {
+  new Vue({
+    router: Router('child'),
+    render: h => h(App),
+  }).$mount('#app')
+} else {
+  console.log('production')
+}
+
