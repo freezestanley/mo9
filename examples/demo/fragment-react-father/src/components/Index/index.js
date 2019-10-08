7c9b7a9271224cf53d8b94ee1caa9401dcc315ae
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-10-08 13:28:33 +0800

    xx

--- examples/demo/fragment-react-father/src/components/Index/index.js
+++ examples/demo/fragment-react-father/src/components/Index/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
-
-
+import './index.scss';
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
-                <a href='/index'>回到home</a>
-                <a href='/home'>去detail</a>
-                <button onClick={() => this.props.history.push('home')}>通过函数跳转</button>
+            <div className={'index'}>
+                <div className={'header'}>众安保险</div>
+                <div className={'body'}>
+                    asdfasdf
+                </div>
+                <div className={'footer'}>
+                    <div>
+                        <img src="https://static.zhongan.com/website/assets/m/v3/home/assets/images/icon/home_on.png" alt="首页"/>
+                        <div>首页</div>
+                    </div>
+                    <div>
+                        <img src="https://static.zhongan.com/website/assets/m/v3/home/assets/images/icon/toutiao.png" alt="头条"/>
+                        <div>头条</div>
+                    </div>
+                    <div>
+                        <img src="https://static.zhongan.com/website/assets/m/v3/home/assets/images/icon/find.png" alt="发现"/>
+                        <div>发现</div>
+                    </div>
+                    <div>
+                        <img src="https://static.zhongan.com/website/assets/m/v3/home/assets/images/icon/my.png" alt="我的"/>
+                        <div>我的</div>
+                    </div>
+                </div>
             </div>
         )
     }
